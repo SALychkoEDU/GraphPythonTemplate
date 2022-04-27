be0346c5b9d723db22d428ac94413f65b1f39bfb
SALychkoEDU <100925864+SALychkoEDU@users.noreply.github.com>
2022-04-28 01:45:03 +0300

    Загружены файлы шаблона

---
+++ graph_path.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+class Graph:
+    
+    def __init__():
+        pass
+    
+    def __getitem__(nodenames) -> float:
+        nodeAname, nodeBname = nodenames
+        return None
+    
+    def __setitem__(self, nodenames, weight):
+        pass
+    
+    def shortestPath(args):
+        """Реализовать метод в соответствии с вариантом
+        Количество находимых путей и способ хранения долж удовлетворять варианту"""
+        pass
